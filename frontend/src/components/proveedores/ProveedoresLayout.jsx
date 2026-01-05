8fa4a48d6ebbbe51078967d6cb24b1059013ca59
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-01-05 17:25:44 -0300

    Hago pruebas y creo el layout pero sin funciones

---
+++ frontend/src/components/proveedores/ProveedoresLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProveedoresLayout() {
+  return (
+    <div>ProveedoresLayout</div>
+  )
+}
+
+export default ProveedoresLayout
---
+++ frontend/src/components/usuarios/UsuariosLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UsuariosLayout() {
+  return (
+    <div>UsuariosLayout</div>
+  )
+}
+
+export default UsuariosLayout
