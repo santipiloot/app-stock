7d7b54e0c61a6c642a1e23451423604b3fe85c20
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-01-06 13:46:48 -0300

    Agregamos botones a la tabla

--- frontend/src/components/proveedores/TablaProveedores.jsx
+++ frontend/src/components/proveedores/TablaProveedores.jsx
@@ -1,6 +1,13 @@
 const proveedores = [{id: 1, nombre: "Jose Maria", telefono: 380412344332, activo: true}, {id: 2, nombre: "Maria Jose", telefono: 380412344332, activo: true}, {id: 3, nombre: "Tapia Chiqui", telefono: 380412344332, activo: false}, {id: 4, nombre: "Chiqui Tapia", telefono: 380412344332, activo: false}]
 
+import { Link } from "react-router"
+
 function TablaProveedores() {
+
+    const handleQuitar = async (id) => {
+
+    }
+
   return (
     <div>
        <h1>Proveedores</h1>
@@ -23,7 +30,9 @@ function TablaProveedores() {
                     <td>{p.activo ? "Si" : "No"}</td>
                     <td>
                         <div>
-                            
+                        <Link className="btn btn-outline-info btn-sm" role="button" to={`/medicos/${p.id}`}> Ver </Link>
+                        <Link className="btn btn-outline-warning btn-sm" role="button" to={`/medicos/${p.id}/modificar`}> Modificar </Link>
+                        <button className="btn btn-outline-danger btn-sm" onClick={() => handleQuitar(p.id)}>Quitar</button>
                         </div>
                     </td>
                 </tr>
