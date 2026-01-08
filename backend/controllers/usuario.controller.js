af721eeaa1c07ecaf3eef6d97d46772c69a1c4dc
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-01-08 12:00:20 -0300

    feat: añadir endpoint para obtener un usuario con su id

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -10,6 +10,26 @@ const usuarioController = {
             console.error("Error en el controlador de getUsuarios:", error);
             res.status(500).json({ success: false, errores: ["Error al obtener los usuarios"] });
         }
+    },
+    getUsuario: async (req, res) => {
+        try {
+            const id = Number(req.params.id);
+
+            const usuario = await Usuario.getUsuario(id);
+
+            if (!usuario) {
+                return res.status(404).json({
+                    success: false,
+                    errores: ["Usuario no encontrado"]
+                });
+            }
+
+            res.status(200).json({ success: true, data: usuario })
+
+        } catch (error) {
+            console.error("Error en el controlador de getUsuario:", error);
+            res.status(500).json({ success: false, errores: ["Error al obtener el usuario"] });
+        }
     }
 }
 
--- backend/models/usuario.model.js
+++ backend/models/usuario.model.js
@@ -1,16 +1,27 @@
 import { db } from "../database.js";
 
 const Usuario = {
-    getUsuarios: async () => { 
-    try {
-        const res = await db.query("SELECT id, nombre, apellido, username, activo, rol_id FROM usuarios ORDER BY id ASC");
-        
-        return res.rows; 
-    } catch (error) {
-        console.error("Error en la base de datos con getUsuarios:", error);
-        throw error; 
+    getUsuarios: async () => {
+        try {
+            const res = await db.query("SELECT id, nombre, apellido, username, activo, rol_id FROM usuarios ORDER BY id ASC");
+
+            return res.rows;
+        } catch (error) {
+            console.error("Error en la base de datos con getUsuarios:", error);
+            throw error;
+        }
+    },
+    getUsuario: async (id) => {
+        try {
+            const res = await db.query(
+                "SELECT id, nombre, apellido, username, activo, rol_id FROM usuarios WHERE id=$1", [id]);
+
+            return res.rows[0]
+        } catch (error) {
+            console.error("Error en la base de datos con getUsuario:", error);
+            throw error;
+        }
     }
-}
 }
 
 export default Usuario
