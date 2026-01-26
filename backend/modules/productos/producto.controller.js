e7af4254ce95936d4b3dc9ec068197db7139d11e
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-01-26 17:58:27 -0300

    feat: agregar endponits para obtener productos con stock critico, otro actualizar stock y otro para actualizar el producto completo

--- backend/modules/productos/producto.controller.js
+++ backend/modules/productos/producto.controller.js
@@ -63,29 +63,103 @@ const productoController = {
         }
     },
     create: async (req, res) => {
-    try {
-        const nuevoProducto = await Producto.create(req.body);
-        res.status(201).json({ 
-            success: true, 
-            message: "Producto creado exitosamente",
-            data: nuevoProducto 
-        });
-    } catch (error) {
-        console.error("Error en create producto:", error);
+        try {
+            const nuevoProducto = await Producto.create(req.body);
+            res.status(201).json({
+                success: true,
+                message: "Producto creado exitosamente",
+                data: nuevoProducto
+            });
+        } catch (error) {
+            console.error("Error en create producto:", error);
 
-        if (error.code === '23505') {
-            return res.status(400).json({ 
-                success: false, 
-                message: "Ya existe un producto con ese código de barras" 
+            if (error.code === '23505') {
+                return res.status(400).json({
+                    success: false,
+                    message: "Ya existe un producto con ese código de barras"
+                });
+            }
+
+            res.status(500).json({
+                success: false,
+                message: "Error interno del servidor al crear el producto"
             });
         }
+    },
+    updateStock: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { stock } = req.body;
 
-        res.status(500).json({ 
-            success: false, 
-            message: "Error interno del servidor al crear el producto" 
-        });
+            const productoActualizado = await Producto.updateStock(id, stock);
+
+            if (!productoActualizado) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Producto no encontrado"
+                });
+            }
+
+            res.status(200).json({
+                success: true,
+                message: "Stock actualizado correctamente",
+                data: productoActualizado
+            });
+        } catch (error) {
+            console.error("Error en updateStock:", error);
+            res.status(500).json({
+                success: false,
+                message: "Error interno del servidor al actualizar el stock"
+            });
+        }
+    },
+    getBajoStock: async (req, res) => {
+        try {
+            const productos = await Producto.getBajoStock();
+            res.status(200).json({
+                success: true,
+                data: productos
+            });
+        } catch (error) {
+            console.error("Error en getBajoStock:", error);
+            res.status(500).json({
+                success: false,
+                message: "Error interno del servidor al obtener productos con bajo stock"
+            });
+        }
+    },
+
+    updateFull: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const productoEditado = await Producto.updateFull(id, req.body);
+
+            if (!productoEditado) {
+                return res.status(404).json({ success: false, message: "Producto no encontrado" });
+            }
+
+            res.status(200).json({
+                success: true,
+                message: "Producto actualizado correctamente por el administrador",
+                data: productoEditado
+            });
+        } catch (error) {
+            console.error("Error en updateFull:", error);
+
+            if (error.code === '23505') {
+                return res.status(400).json({
+                    success: false,
+                    message: "Ya existe otro producto con ese mismo código de barras"
+                });
+            }
+
+            res.status(500).json({
+                success: false,
+                message: "Error interno del servidor al modificar el producto"
+            });
+        }
     }
-}
 };
 
 export default productoController;
--- backend/modules/productos/producto.model.js
+++ backend/modules/productos/producto.model.js
@@ -62,6 +62,42 @@ const Producto = {
 
         const { rows } = await db.query(query, values);
         return rows[0];
+    },
+    updateStock: async (id, nuevoStock) => {
+        const query = `UPDATE productos SET stock = $1 WHERE id = $2 RETURNING *`;
+        const { rows } = await db.query(query, [nuevoStock, id]);
+        return rows[0];
+    },
+    getBajoStock: async () => {
+        const query = `SELECT p.*, c.nombre AS categoria_nombre, prov.nombre AS proveedor_nombre
+        FROM productos p
+        INNER JOIN categorias c ON p.categoria_id = c.id
+        INNER JOIN proveedores prov ON p.proveedor_id = prov.id
+        WHERE p.stock <= p.stock_critico AND p.activo = true
+        ORDER BY p.stock ASC
+    `;
+        const { rows } = await db.query(query);
+        return rows;
+    },
+    updateFull: async (id, datos) => {
+
+        const {nombre, descripcion, codigo_barra, categoria_id,
+            stock, stock_critico, precio, proveedor_id, activo } = datos
+
+        const query = ` UPDATE productos SET nombre = $1, descripcion = $2, codigo_barra = $3, 
+        categoria_id = $4, stock = $5, stock_critico = $6, 
+        precio = $7, proveedor_id = $8, activo = $9
+        WHERE id = $10 
+        RETURNING *
+    `;
+
+        const values = [
+            nombre, descripcion, codigo_barra, categoria_id,
+            stock, stock_critico, precio, proveedor_id, activo, id
+        ];
+
+        const { rows } = await db.query(query, values);
+        return rows[0];
     }
 };
 
