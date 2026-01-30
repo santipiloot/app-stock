40d755bde5e0d2980fa5425d1acd4b7816c79c10
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-01-30 07:16:13 -0300

    Get y Post de pedidos creado

---
+++ backend/modules/pedidos/pedidos.model.js
@@ -0,0 +1,92 @@
+import { db } from "../../config/database";
+
+//Tabla pedidos = id, proveedor_id, usuario_id, fecha_pedido, esstado, precio_total
+
+//Tabla pedido_detalle = id, pedido_id, producto_id, cantidad, precio
+
+//Tabla proveedores = id, nombre, telefono, activo
+
+//Tabla productos = id, nombre, descripcion, codigo_barra, categoria_id, stock, stock_critico, precio, proveedor_id, activo
+
+const Pedido = {
+
+    getPedidos: async () => {
+        const query = `SELECT 
+        pedidos.id,
+        pedidos.fecha_pedido,
+        pedidos.estado,
+        pedido.precio_total,
+        proveedores.nombre AS proveedor_nombre,
+        usuarios.nombre AS usuario_nombre
+        FROM pedidos
+        INNER JOIN proveedores ON pedidos.proveedor_id = proveedores.id
+        INNER JOIN usuarios ON pedidos.usuarios_id = usuarios.id
+        ORDER BY pedidos.id DESC;`;
+
+        const { rows} = await db.query(query);
+        return rows;
+    },
+
+    getPedido: async (id) => {
+
+        const query = `SELECT
+        pd.id,
+        pd.compra_id, 
+        pd.producto_id, 
+        pd.cantidad, 
+        pd.precio, 
+        p.nombre AS producto_nombre 
+        FROM pedido_detalle pd 
+        INNER JOIN productos p ON pd.producto_id = p.id
+        WHERE pd.pedido_id = $1;`;
+
+        const { rows} = await db.query(query, [id]);
+        return rows;
+
+    },
+
+    postPedido: async (datos) => {
+
+        const client = await db.connect();
+
+        try {
+            
+            await client.query("BEGIN");
+
+            const {proveedor_id, usuario_id, fecha_pedido, precio_total, detalles } = datos;
+
+            const queryPedido = `INSERT INTO pedidos (proveedor_id, usuario_id, fecha_pedido, precio_total, estado, precio_total)
+            VALUES ($1, $2, $3, $4, "Pendiente", $5) RETURNING id`;
+
+            const [resPedido] = await client.query(queryPedido, [proveedor_id, usuario_id, fecha_pedido, precio_total]);
+            const pedidoId = resPedido.rows[0].id;
+
+            for (const detalle of detalles) {
+                const queryDetalle = `INSERT INTO pedido_detalle (pedido_id, producto_id, cantidad, precio) VALUES ($1, $2, $3, $4)`;
+
+                await client.query(queryDetalle, [pedidoId, detalle.producto_id, detalle.cantidad, detalle.precio]);
+            }
+
+            await client.query("COMMIT");
+
+            return {success: true, message: "Pedido creado", id: pedidoId};
+
+
+        } catch (error) {
+            await client.query("ROLLBACK");
+            throw error;
+        } finally {
+            client.release();
+        }
+
+    },
+
+    patchEstadoPedido: async (id) => {
+
+        //Hacer patch
+    },
+
+
+}
+
+export default Pedido;
